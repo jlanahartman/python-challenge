fd5db5b2779a5215589dc47b995fc7a7bc7191c1
jlanahartman <122398749+jlanahartman@users.noreply.github.com>
2023-02-13 20:29:45 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,136 +1,84 @@
 
 """
+The total number of votes cast
 
-# loop through the csv file to find: 
-    
-    -The total number of months included in the dataset
+A complete list of candidates who received votes
 
-    -The net total amount of "Profit/Losses" over the entire period
+The percentage of votes each candidate won
 
-    -The changes in "Profit/Losses" over the entire period, and then the average of those changes
+The total number of votes each candidate won
 
-    -The greatest increase in profits (date and amount) over the entire period
-
-    -The greatest decrease in profits (date and amount) over the entire period
+The winner of the election based on popular vote
 """
 
 import os
 import csv
 
-budget_data_csv = os.path.join("Resources", "budget_data.csv")
+election_data_csv = os.path.join("Resources", "election_data.csv")
 
-with open(budget_data_csv, "r") as csvFile:
+with open(election_data_csv, "r") as csvFile:
     csvRead = csv.reader(csvFile, delimiter=",")
 
     next(csvRead)
 
-
-    # Delcare variables 
-    count_months = 0
-    total_revenue = 0
-    monthly_change = 0
-    previous_month = 0
-    total_monthly_change = []
-    max_change = 0
-    min_change = 0
-    max_date = ""
-    min_date = ""
+    candidates = []
+    num_votes = 0
+    vote_counts = []
 
 
     for row in csvRead:
-        count_months = count_months + 1
-     # print(count_months)
-        PL = int(row[1])
-        total_revenue = total_revenue + PL 
-        # previous_month = PL
-        if count_months > 1:
-            monthly_change = PL - previous_month
-            total_monthly_change.append(monthly_change)
-        if monthly_change > max_change:
-            max_change = monthly_change
-            pl_date=row[0]
-            max_date = pl_date
-        if monthly_change < min_change: 
-            min_change = monthly_change
-            pl_date=row[0]
-            min_date = pl_date
 
-            previous_month = PL
 
-      #   avg_profit_loss = sum(monthly_change/len(total_monthly_change))
-      #   avg_profit_loss = round(avg_profit_loss, 2)
-
-        # print(avg_profit_loss)
-        print(max_date)
-
-
-
-    # Print statemnets
-
-    print(f"Financial Analysis") 
-
-    print("-----------------------")
-
-    print(f"Total Months: {count_months}")
-
-    print(f"Total: {total_revenue}")
-
-    print(f"Average Change: {total_monthly_change}")
-
-    print(f"Greatest increase in profits: {max_date}")
-
-    print(f"Greatest increase in profits: {min_date}")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-
-
-
-       
+        num_votes = num_votes + 1
+
+    
+        candidate = row[2]
+
+        if candidate in candidates:
+            candidate_index = candidates.index(candidate)
+            vote_counts[candidate_index] = vote_counts[candidate_index] + 1
+    
+        else:
+            candidates.append(candidate)
+            vote_counts.append(1)
+
+percentages = []
+max_votes = vote_counts[0]
+max_index = 0
+#percentage of vote for each candidate and the winner
+for count in range(len(candidates)):
+    vote_percentage = vote_counts[count]/num_votes*100
+    percentages.append(vote_percentage)
+    if vote_counts[count] > max_votes:
+        max_votes = vote_counts[count]
+        print(max_votes)
+        max_index = count
+    winner = candidates[max_index]
+
+    #print results
+    print("Election Results")
+    print("--------------------------")
+    print(f"Total Votes: {num_votes}")
+    for count in range(len(candidates)):
+        print(f"{candidates[count]}: {percentages[count]}% ({vote_counts[count]})")
+        print("---------------------------")
+        print(f"Winner: {winner}")
+        print("---------------------------")
+
+write_file = f"pypoll_results_summary.txt"
+
+#open write file
+filewriter = open(write_file, mode = 'w')
+
+#print analysis to file
+filewriter.write("Election Results\n")
+filewriter.write("--------------------------\n")
+filewriter.write(f"Total Votes: {num_votes}\n")
+for count in range(len(candidates)):
+    filewriter.write(f"{candidates[count]}: {percentages[count]}% ({vote_counts[count]})\n")
+filewriter.write("---------------------------\n")
+filewriter.write(f"Winner: {winner}\n")
+filewriter.write("---------------------------\n")
+
+#close file
+filewriter.close()
